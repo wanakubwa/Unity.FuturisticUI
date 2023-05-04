6170dceb4df8caab17516a76780d43c1ee4fb11a
wanakubwa <wanakubwa@gmail.com>
2023-05-04 17:47:40 +0200

    Added menu decals animations and improved slider.

---
+++ FuturisticUI/Assets/Scripts/UI/GameUI/Panels/MainMenuPanel/Elements/AnimatedCircle.cs
@@ -0,0 +1,56 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.UI
+{
+    public class AnimatedCircle : MonoBehaviour
+    {
+        #region Fields
+
+        [SerializeField]
+        private Image targetLinesImage;
+        [SerializeField]
+        private float rotateDurationS = 3f;
+        [SerializeField]
+        private Vector3 rotationAngles = new Vector3(0f, 0f, 180f);
+        [SerializeField]
+        private Ease easeType = Ease.InOutSine;
+
+        #endregion
+
+        #region Methods
+
+        private void Start()
+        {
+            RunAnimation();
+        }
+
+        private void RunAnimation()
+        {
+            // Stan default.
+            targetLinesImage.rectTransform.localRotation = Quaternion.identity;
+
+            targetLinesImage.rectTransform.DOLocalRotate(rotationAngles, rotateDurationS, RotateMode.FastBeyond360)
+                .SetEase(easeType)
+                .SetLoops(-1, LoopType.Incremental)
+                .SetLink(gameObject);
+        }
+
+#if UNITY_EDITOR
+
+        private void OnValidate()
+        {
+            // Hot "plug and play".
+            if(Application.isPlaying == true)
+            {
+                targetLinesImage.DOKill();
+                RunAnimation();
+            }
+        }
+
+#endif
+
+        #endregion
+    }
+}
---
+++ FuturisticUI/Assets/Scripts/UI/GameUI/Panels/MainMenuPanel/Elements/AnimatedProgressBar.cs
@@ -0,0 +1,54 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.UI
+{
+    public class AnimatedProgressBar : MonoBehaviour
+    {
+        #region Fields
+
+        [SerializeField]
+        private Image targetImage;
+        [SerializeField]
+        private float fillDurationS = 3f;
+        [SerializeField]
+        private Ease easeType = Ease.InOutSine;
+
+        #endregion
+
+        #region Methods
+
+        private void Start()
+        {
+            RunAnimation();
+        }
+
+        private void RunAnimation()
+        {
+            // Stan default.
+            targetImage.fillAmount = 0;
+
+            targetImage.DOFillAmount(1, fillDurationS)
+                .SetEase(easeType)
+                .SetLoops(-1, LoopType.Yoyo)
+                .SetLink(gameObject);
+        }
+
+#if UNITY_EDITOR
+
+        private void OnValidate()
+        {
+            // Hot "plug and play".
+            if(Application.isPlaying == true)
+            {
+                targetImage.DOKill();
+                RunAnimation();
+            }
+        }
+
+#endif
+
+        #endregion
+    }
+}
